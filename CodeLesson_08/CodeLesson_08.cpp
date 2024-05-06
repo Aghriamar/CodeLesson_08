6da128b297ac5244bc7b385f1c558f8dd480a88e
Aghriamar <A1exXx88@inbox.ru>
2024-05-06 08:37:51 +0300

    Added homework_08

--- CodeLesson_08/CodeLesson_08.cpp
+++ CodeLesson_08/CodeLesson_08.cpp
@@ -1,6 +1,48 @@
 ﻿#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Animal {
+public:
+    virtual void Voice() {
+        std::cout << "Animal says something" << std::endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void Voice() override {
+        std::cout << "Woof!" << std::endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void Voice() override {
+        std::cout << "Meow!" << std::endl;
+    }
+};
+
+class Bird : public Animal {
+public:
+    void Voice() override {
+        std::cout << "Tweet!" << std::endl;
+    }
+};
+
+int main() {
+    const int numAnimals = 3;
+    Animal* animals[numAnimals];
+
+    animals[0] = new Dog();
+    animals[1] = new Cat();
+    animals[2] = new Bird();
+
+    for (int i = 0; i < numAnimals; ++i) {
+        animals[i]->Voice();
+    }
+
+    for (int i = 0; i < numAnimals; ++i) {
+        delete animals[i];
+    }
+
+    return 0;
 }
